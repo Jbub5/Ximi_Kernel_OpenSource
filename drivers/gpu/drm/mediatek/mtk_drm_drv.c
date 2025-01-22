cb5967aacafa52307d84376e4d12ac2c9a898310
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-01-23 01:43:35 +0300

    drivers/gpu/drm/: Import Xiaomi kernel changes

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: YT SHEN <yt.shen@mediatek.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1670,7 +1671,8 @@ static int mtk_drm_fence_release_thread(void *data)
 		DDPINFO("%s:%d wait vblank-\n", __func__, __LINE__);
 
 		mutex_lock(&private->commit.lock);
-		mtk_release_present_fence(private->session_id[0],
+		if (private->session_id[0] > 0)
+			mtk_release_present_fence(private->session_id[0],
 					  atomic_read(&_mtk_fence_idx[0]));
 		mutex_unlock(&private->commit.lock);
 	}

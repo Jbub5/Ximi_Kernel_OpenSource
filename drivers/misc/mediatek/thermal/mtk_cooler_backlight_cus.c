5462f1626523c50094f041fc35152a625a9236f6
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-11-15 02:43:16 +0000

    drivers/misc/mediatek/thermal/: Import Xiaomi changes

--- drivers/misc/mediatek/thermal/mtk_cooler_backlight_cus.c
+++ drivers/misc/mediatek/thermal/mtk_cooler_backlight_cus.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -56,9 +57,10 @@ static int mtk_cl_backlight_set_cur_state
 {
 	int enable = (state == MAX_BACKLIGHT_BRIGHTNESS) ? 0 : 1;
 
+	printk("[%s]: --lyd_thmal, set max brightness = %d\n", __func__, state);
 	setMaxbrightness(state, enable);
 	g_backlight_level = state;
-	mtk_cooler_backlight_dprintk("%s: %d\n", g_backlight_level);
+	mtk_cooler_backlight_dprintk("%u\n", g_backlight_level);
 
 	return 0;
 }
